41b95202980b994c176c9faf501db147d47590ce
adrin35pr <adrin35pro@gmail.com>
2023-07-26 16:08:53 +0300

    Added books and more comfortable menu

--- library_hw/library_hw.cpp
+++ library_hw/library_hw.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <cstdlib>
+
 using namespace std;
 
 
@@ -55,6 +56,8 @@ void TITLE_SCREEN(){
 
 }
 
+
+
 class Book{
     public:
         // Вся інформація про книгу
@@ -75,59 +78,85 @@ class Reader {
 class Library{
     public:
     // 1 - це поки що затичка, треба буде подобавляти книжок і додати функцію "Пожертва", воно як раз і буде фігньою що додає книжки
-        Book library_books[1] = { {"1984", "George Orwell", 1949, }, };
+        Book library_books[5] = { 
+            {"1984", "George Orwell", 1949, },
+            {"Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1997, true},
+            {"To Kill a Mockingbird", "Harper Lee", 1960, false},
+            {"The Great Gatsby", "F. Scott Fitzgerald", 1925, true},
+            {"Pride and Prejudice", "Jane Austen", 1813, false}
+            };
 
 };
 
     int main(){
-        
         TITLE_SCREEN();
 
-        
         string enter_pusher;
         getline(cin, enter_pusher);
+        system("cls");
 
-        if (enter_pusher == "\n"){
-
-            system("cls");
-
-        }
-
+        Library library;
         
+        while (1){
+            
         cout << "1. Library;" << endl;
         cout << "2. Readers" << endl;
+        cout << "0. Exit" << endl;
         
         cout << "Choose by using numbers: ";
         int choice_main_menu;
         cin >> choice_main_menu;
-
+        
         if (choice_main_menu == 1){
-            //тут треба прогрузити весь списко книг, тобто весь клас Library
-            cout << "Choose a book using numbers: " << endl;
+
+            system("cls");
+            for(int j = 0; j < 5; j++){
+                cout << j+1 << " ";
+                cout << library.library_books[j].book_name << ", " << library.library_books[j].book_author << endl;
+            }
+
+            cout << "Choose a book using numbers or push Esc to back to main menu: " << endl;
+                
             int book_number;
             cin >> book_number;
 
-            int i; i = book_number;
-    
-            Library library;
-                cout << "Book: " << library.library_books[i-1].book_name << endl;
-                cout << "Author: " << library.library_books[i-1].book_author << endl;
-                cout << "Year: " << library.library_books[i-1].publication_year << endl;
-                if (library.library_books[i-1].library_status == 0){
-                    cout << "Status: Not enable" << endl;
-                } else if (library.library_books[i-1].library_status == 1){
-                    cout << "Status: Enable" << endl;
+            int i = book_number - 1; 
+
+            cout << "Book: " << library.library_books[i].book_name << endl;
+            cout << "Author: " << library.library_books[i].book_author << endl;
+            cout << "Year: " << library.library_books[i].publication_year << endl;
+            
+            if (library.library_books[i].library_status == false) {
+                cout << "Status: Not enable" << endl;
+            }
+            else if (library.library_books[i].library_status == true) {
+                cout << "Status: Enable" << endl;
+            }
+                
+                cout << "\n" << "Press Enter to continue..." << endl;
+
+                cin.ignore();
+                getline(cin, enter_pusher);
+                
+                if (enter_pusher == "\n") {
+                    system("cls");
+                    continue; 
                 }
-          
+                
             }
 
+            if (choice_main_menu == 2){
 
+                
+
+            }
         
             if (choice_main_menu == 0){
             cout <<  "see you next time";
             sleep(1);
-            return 0;
+            exit(0);
+            break;
             }
-
+        }
         return 0;
     }
